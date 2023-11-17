4814e21ec6eb8451bd7e158600881672be4f36ad
Igor Budarin <jinnfighterwork@yandex.ru>
2023-11-18 00:21:33 +0300

    add: dice roll window logic

---
+++ Assets/Scripts/Client/DiceRoller/ModelDiceRollResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Logic.Services;
+using Reactivity;
+
+namespace Client.DiceRoller
+{
+    public class ModelDiceRollResult : IModelDiceRollResult
+    {
+        public ModelDiceRollResult(IReadOnlyList<RollResult> rollResults, string resultText)
+        {
+            RollResults = rollResults;
+            ResultText = new ReactiveProperty<string>(resultText);
+        }
+
+        public IReadOnlyList<RollResult> RollResults { get; }
+        public IReactiveProperty<string> ResultText { get; }
+    }
+}
---
+++ Assets/Scripts/Client/DiceRoller/ViewDiceRollResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Client.DiceRoller
+{
+    public class ViewDiceRollResult : BaseView
+    {
+        [field: SerializeField] public List<ViewDiceResult> ImagesDice { get; private set; }
+        [field: SerializeField] public TextMeshProUGUI TextResult { get; private set; }
+    }
+}
